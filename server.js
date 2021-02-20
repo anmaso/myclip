a4d84ac2650faccf1370f6744ce56366ec43f97b
Glitch (hello-express) <none>
2021-02-20 16:39:35 +0000

    👭🙈 Checkpoint ./server.js:3429589/4437 ./views/index.html:3429589/20 ./package.json:3429589/14 ./views/index.pug:3429589/285

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
+var bodyParser = require("body-parser");
+var pug = require("pug");
 
 // our default array of dreams
 const dreams = [
@@ -18,6 +20,9 @@ const dict = {};
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.set('view engine', 'pug')
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
@@ -25,33 +30,39 @@ app.get("/", (request, response) => {
 });
 
 app.get("/:key/:value", (request, response) => {
-  const key=request.params.key;
-  const value=request.params.value;
-  dict[key]=value;
-  response.redirect('/'+key);
+  const key = request.params.key;
+  const value = request.params.value;
+  dict[key] = value;
+  response.redirect("/" + key);
 });
-
 
 // send the default array of dreams to the webpage
 app.get("/:key", (request, response) => {
-  const key=request.params.key;
+  const key = request.params.key;
+  response.render('index', { title: 'Hey', message: 'Hello there!' })
+
   // express helps us take JS objects and send them as JSON
-  if (request.headers && request.headers['accept']=='application/json'){
-    return   response.json({[key]:dict[key]});    
+  if (request.headers && request.headers["accept"] == "application/json") {
+    return response.json({ [key]: dict[key] });
   }
-   if (request.headers && (request.headers['user-agent']||'').indexOf('HTML')>=0){
-    response.send('<html><pre>'+dict[key]+JSON.stringify(request.headers));
+  if (
+    request.headers &&
+    (request.headers["user-agent"] || "").indexOf("HTML") >= 0
+  ) {
+    response.send(
+      "<html><pre>" + dict[key] + "\n\n" + JSON.stringify(request.headers)
+    );
   }
   response.send(dict[key]);
 });
 
-app.post("/", (request,response)=>{
-  const key = request.params.key;
-  const value = request.params.value;
-  console.log(request.body)
-  dict[key]=value;
-  response.send('<html><a href="">https://myclip.glitch.me/'+key+'</a>')
-})
+app.post("/", (request, response) => {
+  const key = request.body.key;
+  const value = request.body.value;
+  console.log(request.body);
+  dict[key] = value;
+  response.send('<html><a href="">https://myclip.glitch.me/' + key + "</a>");
+});
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
