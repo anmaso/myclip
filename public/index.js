fbe2fadc3c8d38b8e66095ab68025273da4c5c7d
Glitch (hello-express) <none>
2021-02-21 14:47:24 +0000

    🍍🎯 Checkpoint ./public/index.js:3429589/211 ./views/index.pug:3429589/1251 ./public/style.css:3429589/44 ./views/style.css:3429589/143

--- public/index.js
+++ public/index.js
@@ -2,18 +2,21 @@ var get = function(el){
   return document.getElementById(el);
 }
 
-var decode = function(value, secret){
+var decode = function(){
+  var value = get('value').value;
+  var secret = get('_secret').value;
   get('_value').value=CryptoJS.AES.decrypt(value, secret).toString(CryptoJS.enc.Utf8);
 }
 
 window.onload = function(){
-  
 
-  var value = get('_value').value;
-  var secret = get('_secret').value;
+  var value = get('value').value;
+  
   if(value){
-    decode(value, secret)
+    decode()
   }
+  
+  get('_secret').onkeyup=decode;
   
   get('send').onclick = function(){
   
